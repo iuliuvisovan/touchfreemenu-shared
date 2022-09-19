938964635b0dc477f239a2411ae28551928ffbb4
Iuliu Vișovan <iuliuvisovan@gmail.com>
2022-09-20 00:04:51 +0300

    update shared

---
+++ navigation-logs.d.ts
@@ -0,0 +1,10 @@
+export declare type NavigationLog = {
+    createdAt: string;
+    targetUsername: string;
+    referrer: string;
+    fullUrl: string;
+    geoIpLocation: string;
+    browser: string;
+    device: string;
+    fullInfo: string;
+};
---
+++ src/navigation-logs.ts
@@ -0,0 +1,10 @@
+export type NavigationLog = {
+  createdAt: string
+  targetUsername: string
+  referrer: string
+  fullUrl: string
+  geoIpLocation: string
+  browser: string
+  device: string
+  fullInfo: string
+}
