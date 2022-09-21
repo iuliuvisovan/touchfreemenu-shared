db357686d73424f10f8551d5e56bae7fc5be956a
Iuliu Vișovan <iuliuvisovan@gmail.com>
2022-09-21 11:40:55 +0300

    update shared

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ __exportStar(require("./products"), exports);
 __exportStar(require("./categories"), exports);
 __exportStar(require("./users"), exports);
 __exportStar(require("./navigation-logs"), exports);
+__exportStar(require("./activity-logs"), exports);
